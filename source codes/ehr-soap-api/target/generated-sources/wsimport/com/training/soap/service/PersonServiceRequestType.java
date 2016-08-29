291591cbaac794bb65277fc9ccec45aeaabd7cb1
meveekay <search2cactus@gmail.com>
2016-08-29 11:44:48 -0500

    added source folder
    added source folder from anup

---
+++ source codes/ehr-soap-api/target/generated-sources/wsimport/com/training/soap/service/PersonServiceRequestType.java
@@ -0,0 +1,62 @@
+
+package com.training.soap.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for PersonServiceRequestType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="PersonServiceRequestType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="searchString" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "PersonServiceRequestType", propOrder = {
+    "searchString"
+})
+public class PersonServiceRequestType {
+
+    @XmlElement(required = true)
+    protected String searchString;
+
+    /**
+     * Gets the value of the searchString property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSearchString() {
+        return searchString;
+    }
+
+    /**
+     * Sets the value of the searchString property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSearchString(String value) {
+        this.searchString = value;
+    }
+
+}
