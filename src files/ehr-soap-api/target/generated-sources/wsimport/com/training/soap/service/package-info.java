00d3f59b57f46640c675c91a7b160f73305625e2
meveekay <search2cactus@gmail.com>
2016-08-29 11:39:22 -0500

    added from anup
    added from anup dai

---
+++ src files/ehr-soap-api/target/generated-sources/wsimport/com/training/soap/service/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://com.cubic.net/personservice/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.training.soap.service;
